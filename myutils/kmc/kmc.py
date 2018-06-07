ca4ee626b183b56880d4bbd7f72967d9c423104c
Zdenek Preisler <z.preisler@gmail.com>
2018-06-06 17:17:30 -0700

    KMC plot

--- myutils/kmc/kmc.py
+++ myutils/kmc/kmc.py
@@ -13,6 +13,7 @@ def read_results(folder='sq_field_L_',d=[128,256,512,1024,2048,4096],n='results.
 class data:
     def __init__(self,b):
         from numpy import array
+        self.data=array([(x.data) for x in b])
         self.mean=array([(x.data.mean()) for x in b])
         self.var=array([(x.data.var()) for x in b])
         self.min=array([(x.data.min()) for x in b])
@@ -22,9 +23,30 @@ class kmc:
     """
     Read KMC output
     """
-    def __init__(self,files):
+    def __init__(self,files,delimiter='\t'):
         from myutils import configuration
         from numpy import array
-        self.conf=configuration(files)
+        self.files=files
+        self.conf=configuration(files,delimiter=delimiter)
 
         self.conf.data_key("entropy.dat")
+
+        a,b=self.conf.get_all_pairs('squarewave','entropy.dat',sorted_key=lambda x: float(x[1]))
+        self.amplitude=array([float(x[1]) for x in a])
+        self.entropy=data(b)
+
+        self.L=float(*self.conf.get_values('length')[0])
+
+    def plot(self,attrx='amplitude',attry='entropy'):
+        from matplotlib.pyplot import plot,semilogx,semilogy,figure,xlabel,ylabel,subplots_adjust,errorbar
+        from numpy import sqrt,log
+        x=getattr(self,attrx)
+        y=getattr(self,attry)
+
+        plot(x,y.min,"k-",alpha=0.2,markersize=4,linewidth=1)
+        plot(x,y.max,"k-",alpha=0.2,markersize=4,linewidth=1)
+        plot(x,y.mean,"-")
+        errorbar(x,y.mean,yerr=sqrt(y.var),fmt='.',markersize=3,alpha=0.5)
+        xlabel(r"Amplitude \Theta")
+        ylabel(r"\sigma")
+        subplots_adjust(left=0.19,bottom=0.18)
