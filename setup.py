ead12ff4134a7645fba580d3c97c7ea879d2256d
Zdenek Preisler <z.preisler@gmail.com>
2018-08-28 15:59:33 -0700

    added mof class

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(name='myutils',
         description='utilities',
         author='Zdenek Preisler',
         author_email='z.preisler@gmail.com',
-        packages=['myutils','myutils.einstein','myutils.kmc'],
+        packages=['myutils','myutils.einstein','myutils.kmc',
+            'mof_lattice'],
         scripts=['scripts/fe_einstein_solid.py']
         )
