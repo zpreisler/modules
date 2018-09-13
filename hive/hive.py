059b3edc2fd81747dbc3607a15632d28090aecbf
Zdenek Preisler <z.preisler@gmail.com>
2018-09-12 17:06:44 -0700

    hive

--- hive/hive.py
+++ hive/hive.py
@@ -13,7 +13,7 @@ class hive:
         Data extensions 
         """
 
-        self.attr=['en','mu','rho','epsilon']
+        self.attr=['en','mu','rho','epsilon','pressure']
 
         """
         Input files [files can be e.g. '*.conf']
@@ -21,7 +21,14 @@ class hive:
         self.files=glob(files)
         self.__get_attr__()
 
-        self.save_h5py()
+        self.__get_length__()
+        #self.save_h5py()
+
+    def __get_length__(self):
+        for a in self.attr:
+            attr=self.__name_attr__(a)
+            q=getattr(self,attr)
+            self.length=len(q)
 
     def __get_attr__(self):
         from numpy import fromfile,append
@@ -39,13 +46,14 @@ class hive:
     def __read_attr__(self,name,a):
         from numpy import fromfile,append
         attr=self.__name_attr__(a)
-        p=fromfile(name+a)
+        p=fromfile(name+a,dtype='float32')
         q=getattr(self,attr)
-        setattr(self,attr,append(q,p))
+        offset=int(len(p)/3)
+        setattr(self,attr,append(q,p[offset:]))
 
-    def save_h5py(self):
+    def save_h5py(self,name='data.h5'):
         import h5py
-        self.h5=h5py.File('data.h5','w')
+        self.h5=h5py.File(name,'w')
         for a in self.attr:
             self.h5.create_dataset(a,data=self.__name_attr__(a))
 
